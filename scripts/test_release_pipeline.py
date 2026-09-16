c213f2cc5cd39d09d08a452ea450abd2b53ce9a5
Theekshna Kotian <tkotian@microsoft.com>
2026-09-16 17:52:02 +0530

    Run installer tests in consumer environments

--- scripts/test_release_pipeline.py
+++ scripts/test_release_pipeline.py
@@ -384,6 +384,7 @@ def test_manylinux_repair_does_not_depend_on_odbc(architecture: str, build_odbc:
         "buildPythonWheels": True,
         "buildOdbcNative": build_odbc,
         "buildRustCrates": False,
+        "testPythonWheelInstalls": False,
         "isOfficial": False,
         "publishToFeed": True,
     }
@@ -440,6 +441,7 @@ def test_nonofficial_nuget_versions_follow_python_distribution(
         "buildPythonWheels": True,
         "buildOdbcNative": True,
         "buildRustCrates": False,
+        "testPythonWheelInstalls": False,
         "isOfficial": is_official,
         "publishToFeed": True,
     }
@@ -500,6 +502,7 @@ def test_manylinux_228_builds_use_isolated_cargo_targets(
         "buildPythonWheels": True,
         "buildOdbcNative": True,
         "buildRustCrates": False,
+        "testPythonWheelInstalls": False,
         "isOfficial": False,
         "publishToFeed": False,
     }
@@ -532,6 +535,7 @@ def test_manylinux_228_odbc_builds_enforce_glibc_ceiling(job_name: str) -> None:
         "buildPythonWheels": True,
         "buildOdbcNative": True,
         "buildRustCrates": False,
+        "testPythonWheelInstalls": False,
         "isOfficial": False,
         "publishToFeed": False,
     }
